476e3946cd2e13ea44b9404a5fd0cbffa212d7ed
Aml3t <fischerparsos@gmail.com>
2023-09-13 14:41:20 +0300

    Editing the Main menu

--- BillTimeApp/BillTime/MainWindow.xaml.cs
+++ BillTimeApp/BillTime/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace BillTime
         {
             InitializeComponent();
         }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
