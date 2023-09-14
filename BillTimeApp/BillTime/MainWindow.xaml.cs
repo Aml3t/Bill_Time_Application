081592707115c5f1796549f13bc65c0ed51f114f
Aml3t <fischerparsos@gmail.com>
2023-09-14 12:17:51 +0300

    Editing the Main control

--- BillTimeApp/BillTime/MainWindow.xaml.cs
+++ BillTimeApp/BillTime/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BillTime.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace BillTime
         public MainWindow()
         {
             InitializeComponent();
+            content.Content = new MainControl();
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
