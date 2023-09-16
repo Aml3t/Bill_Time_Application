e4a77ed6897f83900d5b8e2b45336e71ed1b45b5
Aml3t <fischerparsos@gmail.com>
2023-09-16 14:44:37 +0300

    Setting the Data access

--- BillTimeApp/BillTimeLibrary/DataAccess/SqliteDataAccess.cs
+++ BillTimeApp/BillTimeLibrary/DataAccess/SqliteDataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,14 @@ namespace BillTimeLibrary.DataAccess
     {
         public static List<T> LoadData<T>(string sqlStatement, Dictionary<string, object> parameters, string dbName = "Default")
         {
+            DynamicParameters p = new DynamicParameters();
 
+            parameters.ToList().ForEach(x => p.Add(x.Key, x.Value)); // One lines but compact that the other manual labour.
+
+            //foreach (var param in parameters)
+            //{
+            //    p.Add(param.Key, param.Value);
+            //}
         }
     }
 }
