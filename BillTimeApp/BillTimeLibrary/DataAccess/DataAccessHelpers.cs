76088029038191ab94673f66680023d08e0b5c08
Aml3t <fischerparsos@gmail.com>
2023-09-16 14:20:29 +0300

    Setting the Data access

--- BillTimeApp/BillTimeLibrary/DataAccess/DataAccessHelpers.cs
+++ BillTimeApp/BillTimeLibrary/DataAccess/DataAccessHelpers.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BillTimeLibrary.DataAccess
 {
-    internal class DataAccessHelpers
+    public class DataAccessHelpers
     {
+        internal static string LoadCoonnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
     }
 }
--- BillTimeApp/BillTimeLibrary/DataAccess/SqliteDataAccess.cs
+++ BillTimeApp/BillTimeLibrary/DataAccess/SqliteDataAccess.cs
@@ -8,5 +8,9 @@ namespace BillTimeLibrary.DataAccess
 {
     public class SqliteDataAccess
     {
+        public static List<T> LoadData<T>(string sqlStatement, Dictionary<string, object> parameters, string dbName = "Default")
+        {
+
+        }
     }
 }
