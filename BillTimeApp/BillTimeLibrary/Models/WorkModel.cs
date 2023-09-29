778c0cad8a6e28aece7533feb083daa0c3302316
Aml3t <fischerparsos@gmail.com>
2023-09-29 13:04:09 +0300

    Setting the Main control

--- BillTimeApp/BillTimeLibrary/Models/WorkModel.cs
+++ BillTimeApp/BillTimeLibrary/Models/WorkModel.cs
@@ -8,5 +8,12 @@ namespace BillTimeLibrary.Models
 {
     public class WorkModel
     {
+        public int ClientId { get; set; }
+        public double Hours { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; }
+        public bool Paid { get; set; }
+        public int PaymentId { get; set; }
     }
 }
